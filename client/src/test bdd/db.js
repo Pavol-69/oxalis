49502fd5dbeedaa72cdce8e29097f21e8228d353
Pavol-69 <paul.valy@gmail.com>
2024-03-25 20:55:07 +0100

    Update db.js

--- client/src/test bdd/db.js
+++ client/src/test bdd/db.js
@@ -8,7 +8,7 @@ const connection = ADODB.open(
   `Provider=Microsoft.ACE.Oledb.12.0;Data Source=${
     app.isPackaged
       ? path.join(`${__dirname}/../../../../../BDD/testBDD.accdb`)
-      : path.join(`${__dirname}/../../../client/out/BDD/testBDD.accdb`)
+      : path.join(`${__dirname}/../../../server/testBDD.accdb`)
   };`,
   false
 );
